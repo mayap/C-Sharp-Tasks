26c1b2c2f4f4f535b2570bf8c3a4ea81987b6b84
Maya <maia_95@abv.bg>
2015-09-12 13:02:51 +0300

    Create PersonCollection.cs

---
+++ PersonCollection.cs
@@ -0,0 +1,135 @@
+using System;
+using System.Collections.Generic;
+using Wintellect.PowerCollections;
+
+public class PersonCollection : IPersonCollection
+{
+    private Dictionary<string, Person> peopleByEmail = new Dictionary<string, Person>();
+    private Dictionary<string, SortedSet<Person>> peopleByEmailDomain = new Dictionary<string, SortedSet<Person>>();
+    private Dictionary<string, SortedSet<Person>> peopleByNameAndTown = new Dictionary<string, SortedSet<Person>>();
+    private OrderedDictionary<int, SortedSet<Person>> peopleByAge = new OrderedDictionary<int,SortedSet<Person>>();
+    private Dictionary<string, OrderedDictionary<int, SortedSet<Person>>> peopleByTownAndAge = new Dictionary<string, OrderedDictionary<int, SortedSet<Person>>>();
+
+    private string ExtractEmailDomain(string email)
+    {
+        var domain = email.Split('@')[1];
+        return domain;
+    }
+
+    public bool AddPerson(string email, string name, int age, string town)
+    {
+        if (this.FindPerson(email) != null)
+        {
+            return false;
+        }
+
+        var person = new Person() 
+        {
+            Email = email,
+            Age = age,
+            Town = town,
+            Name = name
+        };
+
+        this.peopleByEmail.Add(email, person);
+
+        var emailDomain = this.ExtractEmailDomain(email);
+        this.peopleByEmailDomain.AppendValueToKey(emailDomain, person);
+
+        var nameAndTown = this.CombineNameAndTown(name, town);
+        this.peopleByNameAndTown.AppendValueToKey(nameAndTown,person);
+
+        this.peopleByAge.AppendValueToKey(age, person);
+
+        this.peopleByTownAndAge.EnsureKeyExists(town);
+        this.peopleByTownAndAge[town].AppendValueToKey(age, person);
+
+        return true;
+    }
+
+    public int Count
+    {
+        get
+        {
+            return this.peopleByEmail.Count;
+        }
+    }
+
+    public Person FindPerson(string email)
+    {
+        Person person;
+        var personExists = this.peopleByEmail.TryGetValue(email, out person);
+        return person;
+    }
+
+    public bool DeletePerson(string email)
+    {
+        var person = this.FindPerson(email);
+        if (person == null)
+        {
+            return false;
+        }
+        var personDeleted = this.peopleByEmail.Remove(email);
+
+        var emailDomain = this.ExtractEmailDomain(email);
+        this.peopleByEmailDomain[emailDomain].Remove(person);
+
+        var nameAndTown = this.CombineNameAndTown(person.Name, person.Town);
+        this.peopleByNameAndTown[nameAndTown].Remove(person);
+
+        this.peopleByAge[person.Age].Remove(person);
+
+        peopleByTownAndAge[person.Town][person.Age].Remove(person);
+
+        return true;
+    }
+
+    public IEnumerable<Person> FindPersons(string emailDomain)
+    {
+        return this.peopleByEmailDomain.GetValuesForKey(emailDomain);
+    }
+
+    public IEnumerable<Person> FindPersons(string name, string town)
+    {
+        var NameAndTown = this.CombineNameAndTown(name, town);
+        return this.peopleByNameAndTown.GetValuesForKey(NameAndTown);
+    }
+
+    private string CombineNameAndTown(string name, string town)
+    {
+        const string separator = "|!|";
+        return name + separator + town;
+    }
+
+    public IEnumerable<Person> FindPersons(int startAge, int endAge)
+    {
+        var peopleInRange = this.peopleByAge.Range(startAge,true,endAge,true);
+        foreach (var peopleByAge in peopleInRange)
+        {
+            foreach (var person in peopleByAge.Value)
+            {
+                yield return person;
+            }
+        }
+    }
+
+    public IEnumerable<Person> FindPersons(
+        int startAge, int endAge, string town)
+    {
+        if (!this.peopleByTownAndAge.ContainsKey(town))
+        {
+            yield break;
+        }
+
+        var peopleInRange = this.peopleByTownAndAge[town].Range(startAge, true, endAge, true);
+
+        foreach (var peopleByAge in peopleInRange)
+        {
+            foreach (var person in peopleByAge.Value)
+            {
+                yield return person;
+            }
+        }
+
+    }
+}
